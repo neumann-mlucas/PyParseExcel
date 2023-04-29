0779bcfb5c187f98a6827bc4c67cb28b458cc688
neumann-mlucas <neumann.m.lucas@gmail.com>
2023-04-29 12:45:24 -0300

    add tests

---
+++ tests/test_lexer.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class TestLexer(unittest.TestCase):
+    def test_mix_spacing(self):
+        pass
+
+    def test_number_formats(self):
+        pass
---
+++ tests/test_parser.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TestParser(unittest.TestCase):
+    def test_operator_precedence(self):
+        pass
+
+    def test_function_parameter(self):
+        pass
+
+    def test_parenthesis_expr(self):
+        pass
