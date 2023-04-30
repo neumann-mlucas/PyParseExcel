1ff73eb04e245128cf0f26d09c7e25907b8b390e
neumann-mlucas <neumann.m.lucas@gmail.com>
2023-04-30 00:15:01 -0300

    add tests cases

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,12 +1,59 @@
 import unittest
+from parser import parser
+
+from lexer import lexer
 
 
 class TestParser(unittest.TestCase):
     def test_operator_precedence(self):
-        pass
+        expr = "1 * 2 + 3"
+        tokens = iter(lexer(expr))
+        ast = parser(tokens)
+        assert ast.value == "+"
+        assert ast[1].value == "*"
+
+        expr = "1 * (2 + 3)"
+        tokens = iter(lexer(expr))
+        ast = parser(tokens)
+        assert ast.value == "*"
+        assert ast[1][0].value == "+"
+
+        expr = "1 * (2 + 3) + 4"
+        tokens = iter(lexer(expr))
+        ast = parser(tokens)
+        assert ast.value == "+"
+        assert ast[0].value == 4
 
     def test_function_parameter(self):
-        pass
+        expr = "1 + IF(A1, A2, A3)"
+        tokens = iter(lexer(expr))
+        ast = parser(tokens)
+        assert ast[1].value == "IF"
+        assert len(ast[1].children) == 3
+
+        expr = '1 + IF(A1 * (B2 + C3) , ABS(D4), IF(E5, "YES", "NO"))'
+        tokens = iter(lexer(expr))
+        ast = parser(tokens)
+        assert ast[1].value == "IF"
+        assert len(ast[1].children) == 3
 
     def test_parenthesis_expr(self):
-        pass
+        expr = "(1)"
+        tokens = iter(lexer(expr))
+        ast = parser(tokens)
+        assert ast.value == "("
+        assert ast[0].value == 1
+
+        expr = "((((1) + 2) + 3) + 4) + 5"
+        tokens = iter(lexer(expr))
+        ast = parser(tokens)
+        assert ast[1].value == 5, ast[0].value
+
+        expr = "(1 + 2) + (3 + 4 + 5)"
+        tokens = iter(lexer(expr))
+        ast = parser(tokens)
+        assert ast.value == "+"
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,23 @@
 import unittest
 
+from lexer import lexer
+from utils import parse_function_args, parse_parenthesis_expr
+
 
 class TestUtils(unittest.TestCase):
-    pass
+    def test_parse_parenthesis_expr(self):
+        tokens = iter(lexer("(1 + (2 + 3)) + 4 + 5"))
+        next(tokens)  # parser consumes first LParenthesis
+        parenthesis_expr = list(parse_parenthesis_expr(tokens))
+        assert len(parenthesis_expr) == 7
+        assert len(list(tokens)) == 4
+
+    def test_parse_function_args(self):
+        tokens = iter(lexer("(1 + (2 + 3), 4 + 5, IF(A11, A12))"))
+        args = parse_function_args(tokens)
+        assert len(args) == 3
+        assert tuple(len(arg) for arg in args) == (7, 3, 6)
+
+
+if __name__ == "__main__":
+    unittest.main()
