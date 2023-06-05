f1e18fe4f70ef1053e8df1c9ce918c4f907def82
neumann-mlucas <neumann.m.lucas@gmail.com>
2023-06-05 20:32:05 -0300

    refactor parenthesis utils and add some tests

--- formularesolver/utils.py
+++ formularesolver/utils.py
@@ -1,6 +1,14 @@
 from typing import Iterator
 
-from lexer import LParenthesis, Token
+from lexer import LParenthesis, Token, get_precedence
+
+
+def needs_swap(left_token: Token, right_token: Token) -> bool:
+    "evaluates if the right token has higher precedence than the left token -- aka if a node swap is needed"
+    assert left_token.type == "operator"
+    if right_token.type in ("operator", "function", "symbol"):
+        return get_precedence(left_token) > get_precedence(right_token)
+    return False
 
 
 def parse_parenthesis_expr(tokens: Iterator[Token]) -> Iterator[Token]:
@@ -8,15 +16,19 @@ def parse_parenthesis_expr(tokens: Iterator[Token]) -> Iterator[Token]:
     depth = 0
     for token in tokens:
         match (token.value, depth):
-            case (")", 0):
-                break
+            case _ if depth < 0:
+                raise SyntaxError("Unmatched Parenthesis")
+            case (")", 1):
+                return
             case (")", _):
                 depth -= 1
+            case ("(", 0):
+                depth += 1
+                continue
             case ("(", _):
                 depth += 1
-            case (_, -1):
-                raise SyntaxError("unmatched parenthesis")
         yield token
+    raise SyntaxError("Unmatched Parenthesis")
 
 
 def split_args(tokens: Iterator[Token]) -> Iterator[list[Token]]:
@@ -32,17 +44,12 @@ def split_args(tokens: Iterator[Token]) -> Iterator[list[Token]]:
                 depth += 1
             case (")", _):
                 depth -= 1
-            case (_, -1):
-                raise SyntaxError("unmatched parenthesis")
         buf.append(token)
     yield buf
 
 
 def parse_function_args(tokens: Iterator[Token]) -> list[list[Token]]:
     "parse tokens inside function parenthesis e.g. fn( ... )"
-    lp = next(tokens)
-    assert lp == LParenthesis, lp
-
     args = parse_parenthesis_expr(tokens)
     args = split_args(args)
     return list(args)  # need to consume iterator here
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,17 +6,70 @@ from utils import parse_function_args, parse_parenthesis_expr
 
 class TestUtils(unittest.TestCase):
     def test_parse_parenthesis_expr(self):
-        tokens = iter(lexer("(1 + (2 + 3)) + 4 + 5"))
-        next(tokens)  # parser consumes first LParenthesis
+        expr = "(1)"
+        tokens = iter(lexer(expr))
         parenthesis_expr = list(parse_parenthesis_expr(tokens))
-        assert len(parenthesis_expr) == 7
+        assert len(parenthesis_expr) == 1, parenthesis_expr
+        assert len(list(tokens)) == 0
+
+        expr = "(1 + (2 + 3)) + 4 + 5"
+        tokens = iter(lexer(expr))
+        parenthesis_expr = list(parse_parenthesis_expr(tokens))
+        assert len(parenthesis_expr) == 7, parenthesis_expr
         assert len(list(tokens)) == 4
 
+        expr = "((((1) + 2) + 3) + 4) + 5"
+        tokens = iter(lexer(expr))
+        parenthesis_expr = list(parse_parenthesis_expr(tokens))
+        assert len(parenthesis_expr) == 13
+        assert len(list(tokens)) == 2
+
+        expr = "(1 + 2) + (3 + 4 + 5)"
+        tokens = iter(lexer(expr))
+        parenthesis_expr = list(parse_parenthesis_expr(tokens))
+        assert len(parenthesis_expr) == 3
+        assert parenthesis_expr[1].value == "+"
+        assert len(list(tokens)) == 8
+
+    def test_parse_parenthesis_expr_syntax_error(self):
+        helper = lambda x: list(parse_parenthesis_expr(x))
+
+        for expr in (")(", ")", "(", "((", "))"):
+            tokens = iter(lexer(expr))
+            self.assertRaises(SyntaxError, helper, tokens)
+
+        expr = "((1 + 2) + (3 + 4) + 5"
+        tokens = iter(lexer(expr))
+        self.assertRaises(SyntaxError, helper, tokens)
+
+        expr = "(1 + 2"
+        tokens = iter(lexer(expr))
+        self.assertRaises(SyntaxError, helper, tokens)
+
     def test_parse_function_args(self):
-        tokens = iter(lexer("(1 + (2 + 3), 4 + 5, IF(A11, A12))"))
+        expr = "(1, 2)"
+        tokens = iter(lexer(expr))
+        args = parse_function_args(tokens)
+        assert len(args) == 2
+        assert tuple(len(arg) for arg in args) == (1, 1)
+
+        expr = "(1, (2 + 3))"
+        tokens = iter(lexer(expr))
+        args = parse_function_args(tokens)
+        assert len(args) == 2
+        assert tuple(len(arg) for arg in args) == (1, 5)
+
+        expr = "(1, IF(2 < 3, 4, 5))"
+        tokens = iter(lexer(expr))
+        args = parse_function_args(tokens)
+        assert len(args) == 2
+        assert tuple(len(arg) for arg in args) == (1, 10)
+
+        expr = "(1 + (2 + 3), 4 + 5, IF(A11, A12, A13))"
+        tokens = iter(lexer(expr))
         args = parse_function_args(tokens)
         assert len(args) == 3
-        assert tuple(len(arg) for arg in args) == (7, 3, 6)
+        assert tuple(len(arg) for arg in args) == (7, 3, 8)
 
 
 if __name__ == "__main__":
