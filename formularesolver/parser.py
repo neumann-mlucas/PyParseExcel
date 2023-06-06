c07fab4bba1494113cfe51ddec42d66e277c689e
neumann-mlucas <neumann.m.lucas@gmail.com>
2023-06-06 09:24:52 -0300

    add some tests for parser.py

--- formularesolver/parser.py
+++ formularesolver/parser.py
@@ -1,29 +1,21 @@
-from typing import Iterator
+from itertools import chain
+from typing import Iterator, Self
 
-from lexer import Token, get_precedence
-from utils import parse_function_args, parse_parenthesis_expr
-
-
-def needs_swap(left_token: Token, right_token: Token) -> bool:
-    "evaluates if the right token has higher precedence than the left token -- aka if a node swap is needed"
-    if right_token.type == "operator" or right_token.value == "/":
-        return get_precedence(left_token) > get_precedence(right_token)
-    return False
+from lexer import Token
+from utils import needs_swap, parse_function_args, parse_parenthesis_expr
 
 
 class ASTNode:
     "Abstract Syntax Tree Node"
 
-    def __init__(self, token: Token, children: list | None = None):
-        self.type = token.type
-        self.value = token.value
+    def __init__(self, token: Token, children: list[Self] | None = None):
         self.token = token
         self.children = children if children is not None else []
 
-    def __iter__(self) -> list["ASTNode"]:
+    def __iter__(self) -> list[Self]:
         return self.children
 
-    def __getitem__(self, key) -> "ASTNode":
+    def __getitem__(self, key: int) -> Self:
         return self.children[key]
 
     def __len__(self) -> int:
@@ -37,7 +29,9 @@ class ASTNode:
         children = "".join(
             sorted(node.pprint(depth=depth + 1) for node in self.children)
         )
-        return f"{tabs}<NODE:{self.type.upper()} [{self.value}] >\n{children}"
+        return (
+            f"{tabs}<NODE:{self.token.type.upper()} [{self.token.value}] >\n{children}"
+        )
 
     @property
     def is_end_node(self) -> bool:
@@ -47,52 +41,74 @@ class ASTNode:
     def childless(self) -> bool:
         return self.children == []
 
-    def walk(self):
+    def walk(self) -> Iterator[Token]:
         yield self.token
         for node in self.children:
             yield from node.walk()
 
 
+class VariableNode(ASTNode):
+    pass
+
+
+class ConstantNode(ASTNode):
+    pass
+
+
+class FunctionNode(ASTNode):
+    pass
+
+
+class ParenthesesNode(ASTNode):
+    pass
+
+
 def parser(tokens: Iterator[Token], parent=None) -> ASTNode:
     "consume a list of token to assemble a AST"
 
-    # clean up some call of iter from the code
+    # clean up some call of iter() from the code
     if isinstance(tokens, list):
         tokens = iter(tokens)
 
     try:
         token = next(tokens)
     except StopIteration:
-        return parent
+        return parent  # base case
 
     match token:
-        case ("variable", _) | ("constant", _) | ("boolean", _):
-            node = ASTNode(token, None)
+        case Token("variable"):
+            node = ASTNode(token)
             return parser(tokens, node)
 
-        case ("function", _):
+        case Token("constant"):
+            node = ConstantNode(token)
+            return parser(tokens, node)
+
+        case Token("function"):
             args = parse_function_args(tokens)
             args = [parser(iter(arg)) for arg in args]
-            node = ASTNode(token, args)
+            node = FunctionNode(token, args)
             return parser(tokens, node)
 
-        case ("operator", _):
+        case Token("operator"):
             left_operand = parent
             right_operand = parser(tokens)
 
             # invert nodes if right priority > left priority
             if needs_swap(token, right_operand.token):
                 parent = right_operand
-                node = ASTNode(token, [left_operand, right_operand.children.pop(0)])
+                node = FunctionNode(
+                    token, [left_operand, right_operand.children.pop(0)]
+                )
                 right_operand.children.append(node)
                 return right_operand
 
-            node = ASTNode(token, [left_operand, right_operand])
+            node = FunctionNode(token, [left_operand, right_operand])
             return parser(tokens, node)
 
-        case ("symbol", "("):
-            expr = parse_parenthesis_expr(tokens)
-            node = ASTNode(token, [parser(iter(expr), parent)])
+        case Token("symbol", "("):
+            expr = parse_parenthesis_expr(chain([token], tokens))
+            node = ParenthesesNode(token, [parser(iter(expr), parent)])
             return parser(tokens, node)
 
         case _:
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -9,50 +9,82 @@ class TestParser(unittest.TestCase):
         expr = "1 * 2 + 3"
         tokens = iter(lexer(expr))
         ast = parser(tokens)
-        assert ast.value == "+"
-        assert ast[1].value == "*"
+        assert ast.token.value == "+"
+        assert ast[1].token.value == "*"
 
         expr = "1 * (2 + 3)"
         tokens = iter(lexer(expr))
         ast = parser(tokens)
-        assert ast.value == "*"
-        assert ast[1][0].value == "+"
+        assert ast.token.value == "*"
+        assert ast[1][0].token.value == "+"
 
         expr = "1 * (2 + 3) + 4"
         tokens = iter(lexer(expr))
         ast = parser(tokens)
-        assert ast.value == "+"
-        assert ast[0].value == 4
+        assert ast.token.value == "+"
+        assert ast[0].token.value == 4
+
+        expr = "1 ^ 2 * 3 + 4"
+        tokens = iter(lexer(expr))
+        ast = parser(tokens)
+        assert ast.token.value == "+"
+        assert ast[0].token.value == "*"
+
+        expr = "1 : 2 & 3 + 4"
+        tokens = iter(lexer(expr))
+        ast = parser(tokens)
+        assert ast.token.value == "&"
+        assert ast[0].token.value == "+"
+        assert ast[1].token.value == ":"
 
     def test_function_parameter(self):
         expr = "1 + IF(A1, A2, A3)"
         tokens = iter(lexer(expr))
         ast = parser(tokens)
-        assert ast[1].value == "IF"
+        assert ast[1].token.value == "IF"
         assert len(ast[1].children) == 3
 
         expr = '1 + IF(A1 * (B2 + C3) , ABS(D4), IF(E5, "YES", "NO"))'
         tokens = iter(lexer(expr))
         ast = parser(tokens)
-        assert ast[1].value == "IF"
+        assert ast[1].token.value == "IF"
         assert len(ast[1].children) == 3
 
     def test_parenthesis_expr(self):
         expr = "(1)"
         tokens = iter(lexer(expr))
         ast = parser(tokens)
-        assert ast.value == "("
-        assert ast[0].value == 1
+        assert ast.token.value == "("
+        assert ast[0].token.value == 1
 
         expr = "((((1) + 2) + 3) + 4) + 5"
         tokens = iter(lexer(expr))
         ast = parser(tokens)
-        assert ast[1].value == 5, ast[0].value
+        assert ast[1].token.value == 5, ast[0].value
 
         expr = "(1 + 2) + (3 + 4 + 5)"
         tokens = iter(lexer(expr))
         ast = parser(tokens)
-        assert ast.value == "+"
+        assert ast.token.value == "+"
+
+        expr = ")("
+        tokens = iter(lexer(expr))
+        self.assertRaises(SyntaxError, parser, tokens)
+
+        expr = "((1 + 2) + (3 + 4) + 5"
+        tokens = iter(lexer(expr))
+        self.assertRaises(SyntaxError, parser, tokens)
+
+        expr = "(1 + 2"
+        tokens = iter(lexer(expr))
+        self.assertRaises(SyntaxError, parser, tokens)
+
+    def test_unary_operators(self):
+        expr = "-1"
+        tokens = iter(lexer(expr))
+        ast = parser(tokens)
+        assert ast.token.value == "-"
+        assert ast[0].token.value == 1
 
 
 if __name__ == "__main__":
